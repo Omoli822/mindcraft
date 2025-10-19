5165d0b4762d97dee2ca224beb275da63a6fc848
omoli822 <omoliom@hotmail.com>
2025-10-19 12:03:27 +0800

    2

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ const settings = {
     
     "base_profile": "assistant", // survival, assistant, creative, or god_mode
     "profiles": [
-        // "./andy.json",
+        "./andy.json",
         // "./profiles/gpt.json",
         // "./profiles/claude.json",
         // "./profiles/gemini.json",
@@ -21,7 +21,7 @@ const settings = {
         // "./profiles/deepseek.json",
         // "./profiles/mercury.json",
         // "./profiles/andy-4.json", // Supports up to 75 messages!
-       "./profiles/ollamacloud.json",
+       // "./profiles/ollamacloud.json",
 
         // using more than 1 profile requires you to /msg each bot indivually
         // individual profiles override values from the base profile
